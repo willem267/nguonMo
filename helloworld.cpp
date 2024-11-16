ce527eaad0f5d2e626e3bd5ae40b375f3e5ded0b
willem267 <truong260703@gmail.com>
2024-11-16 13:43:53 +0700

    commit change

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,10 +2,5 @@
 using namespace std;
 
 int main (){
-    cout<<"nhap so a: \n";
-    int a, b;
-    cin>>a;
-    cout<<"Nhap so b: \n";
-    cin>>b;
-    cout<<"tong cua a va b la: "<< a+b;
+    cout<<"HELLO WORLD!";
 }
