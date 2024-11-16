a289ade17227c95cb5daa17c25d0401d862dbd3c
willem267 <truong260703@gmail.com>
2024-11-16 13:37:48 +0700

    test'

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout<<"nhap so a: \n";
+    int a, b;
+    cin>>a;
+    cout<<"Nhap so b: \n";
+    cin>>b;
+    cout<<"tong cua a va b la: "<< a+b;
+}
