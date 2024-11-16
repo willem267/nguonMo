89631733cbd99f77f0b2db53b383f249da48d5f8
TAT <92031654+TAT191103@users.noreply.github.com>
2024-11-16 14:05:14 +0700

    Update helloworld.cpp dep hon
    nhin cho no dep hon

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main (){
  
-    cout<<"nhap so a: \n";
     int a, b;
+ 
+    cout<<"nhap so a: \n";
     cin>>a;
     cout<<"Nhap so b: \n";
     cin>>b;
+ 
     cout<<"tong cua a va b la: "<< a+b;
     cout<<"\n";
-
 }
